8079fbe349f87ee29dba71be2c00b8969c1295bb
MACARENASTORE <oclusiva9204@gmail.com>
2023-06-26 22:42:19 -0500

    taller v0.0.0.7

--- test/paciente.test.ts
+++ test/paciente.test.ts
@@ -1,45 +1,57 @@
 import request from "supertest";
-import app from "../src/app";
+import App from "../src/app";
 
+describe("Paciente API", () => {
+  let app: App;
 
-test("Debe obtener un código de respuesta exitoso al obtener todos los pacientes", async () => {
-  const response = await request(app).get("/paciente");
-  expect(response.statusCode).toBe(200);
-}, 20000);
+  beforeAll(() => {
+    app = new App();
+    app.start();
+  });
 
-test("Debe crear un nuevo paciente", async () => {
-  const pacienteData = {
-    cedula: 123456789,
-    nombre: "John",
-    apellido: "Doe",
-    fecha: "1990-01-01",
-    telefono: "1234567890",
-  };
+  afterAll(() => {
+    app.close();
+  });
 
-  const response = await request(app)
-    .post("/crear_paciente")
-    .send(pacienteData);
-  expect(response.statusCode).toBe(200);
-  expect(response.body).toHaveProperty("cedula", 123456789);
-}, 20000);
+  test("GET /paciente - Debe obtener una respuesta exitosa al obtener todos los pacientes", async () => {
+    const response = await request(app.app).get("/paciente");
+    expect(response.status).toBe(200);
+  }, 20000);
 
-test("Debe actualizar un paciente existente", async () => {
-  const pacienteData = {
-    cedula: 123456789,
-    nombre: "John",
-    apellido: "Doe",
-    fecha: "1990-01-01",
-    telefono: "1234567890",
-  };
+  test("POST /crear_paciente - Debe crear un nuevo paciente", async () => {
+    const pacienteData = {
+      cedula: 123456789,
+      nombre: "John",
+      apellido: "Doe",
+      fecha: "1990-01-01",
+      telefono: "1234567890",
+    };
 
-  const response = await request(app)
-    .put("/actualizar_paciente/123456789")
-    .send(pacienteData);
-  expect(response.statusCode).toBe(200);
-  expect(response.body).toHaveProperty("nombre", "John");
-}, 20000);
+    const response = await request(app.app)
+      .post("/crear_paciente")
+      .send(pacienteData);
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("cedula", 123456789);
+  }, 20000);
 
-test("Debe eliminar un paciente existente", async () => {
-  const response = await request(app).delete("/eliminar_paciente/123456789");
-  expect(response.statusCode).toBe(200);
-}, 20000);
+  test("PUT /actualizar_paciente/:cedula - Debe actualizar un paciente existente", async () => {
+    const pacienteData = {
+      cedula: 123456789,
+      nombre: "John",
+      apellido: "Doe",
+      fecha: "1990-01-01",
+      telefono: "1234567890",
+    };
+
+    const response = await request(app.app)
+      .put("/actualizar_paciente/123456789")
+      .send(pacienteData);
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("nombre", "John");
+  }, 20000);
+
+  test("DELETE /eliminar_paciente/:cedula - Debe eliminar un paciente existente", async () => {
+    const response = await request(app.app).delete("/eliminar_paciente/123456789");
+    expect(response.status).toBe(200);
+  }, 20000);
+});
